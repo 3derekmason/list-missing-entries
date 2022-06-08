4bcca8b740839988f005cbf92e38c57ef45195f1
Derek Mason <3derekmason@gmail.com>
2022-06-08 17:33:55 -0600

    list missing entries passing init tests

--- listMissingEntries.js
+++ listMissingEntries.js
@@ -1,3 +1,19 @@
-const listMissingEntries = () => {};
+const listMissingEntries = (data) => {
+  if (Object.keys(data).length === 0) {
+    return "Input must be an object of data...";
+  }
+  let missing = [];
+  let keys = Object.keys(data);
+  keys.forEach((key) => {
+    if (!data[key] || data[key].length === 0) {
+      missing.push(key);
+    }
+  });
+  if (missing.length > 0) {
+    return `${missing.length} fields missing, ${JSON.stringify(missing)}`;
+  } else {
+    return "No missing fields";
+  }
+};
 
 export default listMissingEntries;
