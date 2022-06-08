d30cac49f51b9161c6a6a271b260b895dcdd650e
Derek Mason <3derekmason@gmail.com>
2022-06-08 17:35:44 -0600

    comment for clarity

--- listMissingEntries.js
+++ listMissingEntries.js
@@ -1,14 +1,18 @@
 const listMissingEntries = (data) => {
+  // Because you can't always rely on typeof...
   if (Object.keys(data).length === 0) {
     return "Input must be an object of data...";
   }
+  // Store missing fields to return
   let missing = [];
   let keys = Object.keys(data);
   keys.forEach((key) => {
+    // if value at key is falsey (or if array is empty) add to missing collection
     if (!data[key] || data[key].length === 0) {
       missing.push(key);
     }
   });
+  // Return string with result missing data
   if (missing.length > 0) {
     return `${missing.length} fields missing, ${JSON.stringify(missing)}`;
   } else {
